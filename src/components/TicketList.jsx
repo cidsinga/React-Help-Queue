84c82f0e45a9bd7fb1e3dd8e32503ca4fad5b5d6
Student <example@example.com>
2020-02-11 10:12:45 -0800

    initialize project

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Ticket from "./Ticket"
+
+var masterTicketList = [
+  {
+    names: "Casey & Tyler" ,
+    location: "7B",
+    issue: "all your bases are belong to me"
+  },
+  {
+    names: 'Thanos & Gamorrah',
+    location: '3A',
+    issue: 'The Avengers'
+  },
+  {
+    names: 'Imani & Jacob',
+    location: '9F',
+    issue: 'Donkey picture not displaying on hover in Zoology app. :('
+  },
+]
+
+
+function TicketList(){
+  return (
+      <div>
+        <hr/>
+        {masterTicketList.map((ticket, index) =>
+          <Ticket names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue} />
+        )}
+      </div>
+    );
+  }
+
+  export default TicketList;
